356320d65673fb40e0f712ef34d6fc7ee00c1e08
Warit Santaputra <waritsan@gmail.com>
2018-06-27 17:41:10 +0700

    Add metrices

--- 06-intro-6/mpow.glsl
+++ 06-intro-6/mpow.glsl
@@ -1,5 +1,5 @@
-mat2 matrixPower(highp mat2 m, int n) {
-  
+mat2 matrixPower(highp mat2 m1, int n) {
+
   //Raise the matrix m to nth power
 
   // For example:
@@ -7,7 +7,29 @@ mat2 matrixPower(highp mat2 m, int n) {
   //  matrixPower(m, 2) = m * m
   //
 
-  return mat2(1.0);  
+  mat2 m2 = m1 * m1;
+  mat2 m4 = m2 * m2;
+  mat2 m8 = m4 * m4;
+
+  mat2 result = mat2(1.0);
+
+  if(n >= 8) {
+    n -= 8;
+    result *= m8;
+  }
+  if(n >= 4) {
+    n -= 4;
+    result *= m4;
+  }
+  if(n >= 2) {
+    n -= 2;
+    result *= m2;
+  }
+  if(n >= 1) {
+    result *= m1;
+  }
+
+  return result;
 }
 
 //Do not change this line or the name of the above function
