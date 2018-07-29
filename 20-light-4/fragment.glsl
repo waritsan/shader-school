d97aef24a95c587a6071346a72f327583506da1c
Warit Santaputra <waritsan@gmail.com>
2018-07-29 17:01:19 +0700

    Add multiple lights

--- 20-light-4/fragment.glsl
+++ 20-light-4/fragment.glsl
@@ -16,6 +16,18 @@ uniform vec3 lightPosition;
 
 uniform float shininess;
 
+varying vec3 fragNormal;
+varying vec3 fragPosition;
+varying vec3 lightDirection;
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec3 eyeDirection = normalize(fragPosition);
+  vec3 normal = normalize(fragNormal);
+  vec3 light = normalize(lightDirection);
+  
+  float lambert = max(dot(normal, light), 0.0);
+  float phong = pow(max(dot(reflect(light, normal), eyeDirection), 0.0), shininess);
+
+  vec3 lightColor = ambient + diffuse * lambert + specular * phong;
+  gl_FragColor = vec4(lightColor, 1);
 }
--- 21-light-5/fragment.glsl
+++ 21-light-5/fragment.glsl
@@ -14,6 +14,21 @@ uniform vec3 ambient;
 
 uniform PointLight lights[4];
 
+varying vec3 fragNormal;
+varying vec3 fragPosition;
+varying vec3 lightDirection[4];
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec3 eyeDirection = normalize(fragPosition);
+  vec3 normal = normalize(fragNormal);
+  vec3 lightColor = ambient;
+
+  for(int i=0; i<4; ++i) {
+    vec3 light = normalize(lightDirection[i]);
+    float lambert = max(dot(normal, light), 0.0);
+    float phong = pow(max(dot(reflect(light, normal), eyeDirection), 0.0), lights[i].shininess);
+    lightColor += lambert * lights[i].diffuse + phong * lights[i].specular;
+  }
+  
+  gl_FragColor = vec4(lightColor, 1);
 }
